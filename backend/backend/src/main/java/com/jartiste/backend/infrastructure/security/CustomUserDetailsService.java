f7871f586a49dab9a05f7c4e1fe2526ad3c7122b
Coderyounes <younesanas2021@gmail.com>
2026-01-20 15:52:33 +0100

    add CustomUserDetailsService for loading user details by username

---
+++ backend/backend/src/main/java/com/jartiste/backend/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.jartiste.backend.infrastructure.security;
+
+import com.jartiste.backend.domain.entity.User;
+import com.jartiste.backend.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new AuthenticatedUser(user);
+    }
+}
