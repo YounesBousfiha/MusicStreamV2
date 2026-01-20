211d9875557d6f08e9efedd659f1deae79945707
Coderyounes <younesanas2021@gmail.com>
2026-01-20 20:57:37 +0100

    add songFile and coverFile fields to SongRequest with validation

--- backend/backend/src/main/java/com/jartiste/backend/presentation/dto/request/SongRequest.java
+++ backend/backend/src/main/java/com/jartiste/backend/presentation/dto/request/SongRequest.java
@@ -2,10 +2,16 @@ package com.jartiste.backend.presentation.dto.request;
 
 
 import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record SongRequest(
         String title,
         String artist,
-        int duration) {
+
+        @NotNull(message = "Duration must be provided")
+        MultipartFile songFile,
+        MultipartFile coverFile
+        ) {
 }
