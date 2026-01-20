9ab3349ee5f31c2747a43e4d35fe42cf79310061
Coderyounes <younesanas2021@gmail.com>
2026-01-20 22:10:19 +0100

    add getAllSongs method to ISongService for retrieving all songs

--- backend/backend/src/main/java/com/jartiste/backend/application/service/ISongService.java
+++ backend/backend/src/main/java/com/jartiste/backend/application/service/ISongService.java
@@ -3,7 +3,10 @@ package com.jartiste.backend.application.service;
 import com.jartiste.backend.presentation.dto.request.SongRequest;
 import com.jartiste.backend.presentation.dto.response.SongResponse;
 
+import java.util.List;
+
 public interface ISongService {
 
-    public SongResponse save(SongRequest request);
+    SongResponse save(SongRequest request);
+    List<SongRequest> getAllSongs();
 }
