c06209ae55a6ed96b88ca62f43cb1c0fa2cbe391
Coderyounes <younesanas2021@gmail.com>
2026-01-20 17:02:58 +0100

    add global exception handlers for UsernameNotFoundException and UserAlreadyExistsException

--- backend/backend/src/main/java/com/jartiste/backend/presentation/adviser/GlobalExceptionHandler.java
+++ backend/backend/src/main/java/com/jartiste/backend/presentation/adviser/GlobalExceptionHandler.java
@@ -1,8 +1,47 @@
 package com.jartiste.backend.presentation.adviser;
 
 
+import com.jartiste.backend.domain.exception.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.net.URI;
+import java.time.Instant;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ProblemDetail handleUsernameNotFoundException(UsernameNotFoundException ex) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, ex.getMessage()
+        );
+
+        problemDetail.setTitle("User Not Found");
+        problemDetail.setProperty("timestamp", Instant.now());
+        problemDetail.setProperty("errorCode", HttpStatus.NOT_FOUND.value());
+
+        return problemDetail;
+    }
+
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ProblemDetail handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT, ex.getMessage()
+        );
+
+        problemDetail.setTitle("Account Registration Failed");
+        problemDetail.setProperty("timestamp", Instant.now());
+        problemDetail.setProperty("errorCode", HttpStatus.CONFLICT.value());
+
+
+        return problemDetail;
+    }
 }
