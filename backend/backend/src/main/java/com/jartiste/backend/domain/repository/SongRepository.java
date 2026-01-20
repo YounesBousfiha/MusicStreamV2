ae6ff0e2018c0aeab61e88234b7146b7abd1ff89
Coderyounes <younesanas2021@gmail.com>
2026-01-20 20:57:32 +0100

    add import for List in SongRepository

--- backend/backend/src/main/java/com/jartiste/backend/domain/repository/SongRepository.java
+++ backend/backend/src/main/java/com/jartiste/backend/domain/repository/SongRepository.java
@@ -5,6 +5,7 @@ import com.jartiste.backend.domain.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
 }
