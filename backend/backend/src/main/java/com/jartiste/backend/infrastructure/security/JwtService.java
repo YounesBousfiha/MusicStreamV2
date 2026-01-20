ea45fd09cfc208556c6fc5ccb4862ef5189fc421
Coderyounes <younesanas2021@gmail.com>
2026-01-20 20:41:01 +0100

    add methods for token validation and extraction in JwtService

--- backend/backend/src/main/java/com/jartiste/backend/infrastructure/security/JwtService.java
+++ backend/backend/src/main/java/com/jartiste/backend/infrastructure/security/JwtService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -32,12 +33,22 @@ public class JwtService {
     }
 
     public String extractUsername(String token) {
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        return getDecodedJWT(token).getSubject();
+    }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getDecodedJWT(token).getExpiresAt().before(new Date());
+    }
+
+    private DecodedJWT getDecodedJWT(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
         JWTVerifier verifier = JWT.require(algorithm).build();
 
-        DecodedJWT decodedJWT = verifier.verify(token);
-
-        return decodedJWT.getSubject();
+        return verifier.verify(token);
     }
 }
