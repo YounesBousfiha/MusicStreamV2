eddf58bf614f596eaa8b5998465470529689443d
Coderyounes <younesanas2021@gmail.com>
2026-01-20 22:09:19 +0100

    implement save and getAllSongs methods in SongService for song management

--- backend/backend/src/main/java/com/jartiste/backend/application/service/impl/SongService.java
+++ backend/backend/src/main/java/com/jartiste/backend/application/service/impl/SongService.java
@@ -1,11 +1,16 @@
 package com.jartiste.backend.application.service.impl;
 
+import com.jartiste.backend.application.mapper.SongMapper;
 import com.jartiste.backend.application.service.ISongService;
+import com.jartiste.backend.domain.entity.Song;
 import com.jartiste.backend.domain.repository.SongRepository;
+import com.jartiste.backend.infrastructure.storage.IFileStorageService;
 import com.jartiste.backend.presentation.dto.request.SongRequest;
 import com.jartiste.backend.presentation.dto.response.SongResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +18,31 @@ public class SongService  implements ISongService {
 
 
     private final SongRepository songRepository;
+    private final IFileStorageService fileStorageService;
+    private final SongMapper songMapper;
 
     @Override
     public SongResponse save(SongRequest request) {
-        return null;
+        String songFileName = this.fileStorageService.save(request.songFile());
+
+        String coverFileName = null;
+
+        if(request.coverFile() != null && !request.coverFile().isEmpty()) {
+            coverFileName = this.fileStorageService.save(request.coverFile());
+        }
+
+        Song song = songMapper.toEntity(request);
+
+        song.setSongUrl(songFileName);
+        song.setCoverUrl(coverFileName);
+
+        Song savedSong = this.songRepository.save(song);
+
+        return songMapper.toResponse(savedSong);
+    }
+
+    @Override
+    public List<SongRequest> getAllSongs() {
+        return List.of();
     }
 }
