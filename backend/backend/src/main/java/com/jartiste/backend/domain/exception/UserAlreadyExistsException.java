b31573d8cd26e064ea72c149752c63a2e68affe4
Coderyounes <younesanas2021@gmail.com>
2026-01-20 17:02:10 +0100

    add UserAlreadyExistsException for handling user registration conflicts

---
+++ backend/backend/src/main/java/com/jartiste/backend/domain/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.jartiste.backend.domain.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
