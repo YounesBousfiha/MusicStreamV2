0ab00161ed64d4497b9ba8af9f4533d6fd1b6ccb
Coderyounes <younesanas2021@gmail.com>
2026-01-22 18:08:32 +0100

    add createSong method to SongController for saving songs and implement getAllSongs and serveFile methods

--- backend/backend/src/main/java/com/jartiste/backend/presentation/controller/SongController.java
+++ backend/backend/src/main/java/com/jartiste/backend/presentation/controller/SongController.java
@@ -1,9 +1,21 @@
 package com.jartiste.backend.presentation.controller;
 
 import com.jartiste.backend.application.service.ISongService;
+import com.jartiste.backend.infrastructure.storage.IFileStorageService;
+import com.jartiste.backend.presentation.dto.request.SongRequest;
+import com.jartiste.backend.presentation.dto.response.SongResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +23,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class SongController {
 
     private final ISongService songService;
+    private final IFileStorageService fileStorageService;
+
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize( "hasRole('ADMIN')")
+    public ResponseEntity<SongResponse> createSong(SongRequest request) {
+        return ResponseEntity.ok(songService.save(request));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<SongResponse>> getAllSongs() {
+        return ResponseEntity.ok(songService.getAllSongs());
+    }
+
+
+    @GetMapping("/file/{fileName:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String fileName) {
+
+        Resource resource = this.fileStorageService.load(fileName);
+
+        String contentType = "application/octet-stream";
+        try {
+            if(resource.getFile().exists()) {
+                contentType = Files.probeContentType(resource.getFile().toPath());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read file: " + fileName);
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 }
