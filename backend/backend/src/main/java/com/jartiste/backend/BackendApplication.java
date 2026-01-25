0368ef0b1b57867281310402486431a2b1a483cd
Coderyounes <younesanas2021@gmail.com>
2026-01-25 14:41:24 +0100

    add AppStartupSanityCheck initializer to BackendApplication for startup validation

--- backend/backend/src/main/java/com/jartiste/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/jartiste/backend/BackendApplication.java
@@ -1,5 +1,9 @@
 package com.jartiste.backend;
 
+import com.jartiste.backend.infrastructure.config.AppStartupSanityCheck;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		app.addInitializers(new AppStartupSanityCheck());
+		app.run(args);
 	}
 
 }
