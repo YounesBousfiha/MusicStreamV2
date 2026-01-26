714be1b73e9d8336b4a81e2b8f1edcb87a4c1036
Coderyounes <younesanas2021@gmail.com>
2026-01-26 09:49:33 +0100

    remove unused CustomUserDetailsService and related method from ApplicationConfig

--- backend/backend/src/main/java/com/jartiste/backend/infrastructure/config/ApplicationConfig.java
+++ backend/backend/src/main/java/com/jartiste/backend/infrastructure/config/ApplicationConfig.java
@@ -1,27 +1,17 @@
 package com.jartiste.backend.infrastructure.config;
 
 
-import com.jartiste.backend.infrastructure.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
-    private final CustomUserDetailsService userDetailsService;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return this.userDetailsService;
-    }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
