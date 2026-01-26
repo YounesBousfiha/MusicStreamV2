909031aa4667266c07df1af3cbe6cc62ed6a3a74
Coderyounes <younesanas2021@gmail.com>
2026-01-26 09:49:49 +0100

    update SecurityConfig to permit all requests and simplify authorization rules

--- backend/backend/src/main/java/com/jartiste/backend/infrastructure/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/jartiste/backend/infrastructure/config/SecurityConfig.java
@@ -36,9 +36,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers("/auth/**").permitAll()
-                                .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
+                        authorize.anyRequest().permitAll()
                         )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
