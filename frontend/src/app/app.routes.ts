2bd722389e2ed521859c34ed0e5689acf1fbb566
Coderyounes <younesanas2021@gmail.com>
2026-01-30 19:36:22 +0100

    add admin routes with guard for managing songs and uploads

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {AuthLayout} from './layout/auth-layout/auth-layout';
 import {Login} from './features/auth/login/login';
 import {Register} from './features/auth/register/register';
 import {authGuard} from './core/guards/auth.guard';
+import {adminGuard} from './core/guards/admin.guard';
 
 export const routes: Routes = [
   {
@@ -21,6 +22,26 @@ export const routes: Routes = [
         path: 'home',
         component: Home
       }
+    ]
+  },
+  {
+    path: 'admin',
+    canActivate: [adminGuard],
+    loadComponent: () => import('./features/admin/admin-layout/admin-layout').then(m => m.AdminLayout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'songs',
+        pathMatch: 'full'
+      },
+      {
+        path: 'songs',
+        loadComponent: () => import('./features/admin/songs-list/songs-list').then(m => m.SongsList)
+      },
+      {
+        path: 'upload',
+        loadComponent: () => import('./features/admin/song-upload/song-upload').then(m => m.SongUpload)
+      },
     ]
   },
   {
