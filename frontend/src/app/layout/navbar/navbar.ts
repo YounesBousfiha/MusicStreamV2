23a56c57d5063e6c18b2466cc28b35bd4222bb5c
Coderyounes <younesanas2021@gmail.com>
2026-01-30 14:43:57 +0100

    add dropdown toggle and logout functionality to navbar component

--- frontend/src/app/layout/navbar/navbar.ts
+++ frontend/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {AuthStore} from '../../core/store/auth.store';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
+  authStore = inject(AuthStore);
+
+  isDropdownOpen = signal(false);
+
+
+  toggleDropdown() {
+    this.isDropdownOpen.update(value => !value);
+  }
+
+  logout() {
+    this.authStore.logout();
+    this.isDropdownOpen.set(false);
+  }
 }
