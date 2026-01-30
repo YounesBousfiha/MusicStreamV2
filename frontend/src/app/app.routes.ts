d90d6abad4b45c4e9f4be829be6d469fd333af2c
Coderyounes <younesanas2021@gmail.com>
2026-01-30 14:42:56 +0100

    add routing for home and authentication features with guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,36 @@
 import { Routes } from '@angular/router';
 import {MainLayout} from './layout/main-layout/main-layout';
+import {Home} from './features/home/home';
+import {AuthLayout} from './layout/auth-layout/auth-layout';
+import {Login} from './features/auth/login/login';
+import {Register} from './features/auth/register/register';
+import {authGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
-    children: []
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    component: AuthLayout,
+    children: [
+      { path: 'login', component: Login},
+      { path: 'register', component: Register},
+      { path: '', redirectTo: 'login', pathMatch: 'full'}
+    ]
   }
 
   ];
