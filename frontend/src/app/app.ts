2e60c5cfcf685f25c19018dff5c002422c0fda0e
Coderyounes <younesanas2021@gmail.com>
2026-01-31 01:21:49 +0100

    inject PlayerStore into app component and initialize audio events in ngOnInit

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Toast} from './shared/components/toast/toast';
+import {PlayerStore} from './core/store/player.store';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import {Toast} from './shared/components/toast/toast';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('frontend');
+
+  readonly playerStore = inject(PlayerStore);
+
+  ngOnInit() {
+    this.playerStore.initAudioEvents();
+  }
 }
