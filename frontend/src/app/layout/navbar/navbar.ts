2939e279bdb582ff9da80e555ca6ce9a02031350
Coderyounes <younesanas2021@gmail.com>
2026-01-31 13:14:12 +0100

    add search bar to navbar with onSearch method to filter songs, improve layout responsiveness and admin area icon

--- frontend/src/app/layout/navbar/navbar.ts
+++ frontend/src/app/layout/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import {Component, computed, inject, signal} from '@angular/core';
 import {AuthStore} from '../../core/store/auth.store';
 import {RouterLink} from '@angular/router';
+import {SongStore} from '../../core/store/song.store';
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +14,7 @@ import {RouterLink} from '@angular/router';
 export class Navbar {
 
   authStore = inject(AuthStore);
-
+  songStore = inject(SongStore);
   isDropdownOpen = signal(false);
   readonly isAdmin = computed(() => this.authStore.user()?.role === 'ADMIN');
 
@@ -25,5 +26,10 @@ export class Navbar {
   logout() {
     this.authStore.logout();
     this.isDropdownOpen.set(false);
+  }
+
+  onSearch(event: any) {
+    const query = event.target.value;
+    this.songStore.updateFilter(query);
   }
 }
