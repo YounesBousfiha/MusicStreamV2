470740479999fa04fdc8428d255f9a52d6c9cd0b
Coderyounes <younesanas2021@gmail.com>
2026-01-31 01:22:00 +0100

    inject PlayerStore into Home component

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {SongCard} from '../../shared/components/song-card/song-card';
 import {SongStore} from '../../core/store/song.store';
 import {AuthStore} from '../../core/store/auth.store';
+import {PlayerStore} from '../../core/store/player.store';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,7 @@ import {AuthStore} from '../../core/store/auth.store';
 export class Home implements OnInit{
   readonly songStore = inject(SongStore);
   readonly  authStore = inject(AuthStore);
-
+  readonly playerStore = inject(PlayerStore);
   ngOnInit() {
     if(this.songStore.songs().length === 0) {
       this.songStore.loadAll();
