e1142a919a812b6454bec45a07d49792ff591d19
Coderyounes <younesanas2021@gmail.com>
2026-02-02 11:13:32 +0100

    add unit tests for ToastService and SongCard component, configure Jasmine types, and update test dependencies

--- frontend/src/app/shared/components/song-card/song-card.spec.ts
+++ frontend/src/app/shared/components/song-card/song-card.spec.ts
@@ -1,23 +1,73 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SongCard } from './song-card';
+import {By} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {PlayerStore} from '../../../core/store/player.store';
+import {SongCard} from './song-card';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {signal} from '@angular/core';
 
 describe('SongCard', () => {
   let component: SongCard;
   let fixture: ComponentFixture<SongCard>;
 
+  const mockPlayerStore = {
+    play: (song: any) => {},
+    currentSong: signal(null),
+    isPlaying: signal(false)
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SongCard]
+      imports: [SongCard],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: PlayerStore, useValue: mockPlayerStore }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SongCard);
     component = fixture.componentInstance;
+
+    component.data = mockSong;
+    fixture.detectChanges();
+
     await fixture.whenStable();
   });
+
+  const mockSong = {
+    id: 1,
+    title: 'Test Song Title',
+    artist: 'Test Artist',
+    cover: 'cover.jpg',
+    songUrl: 'song.mp3',
+    duration: 500,
+    createdAt: "",
+    coverUrl: 'http://localhost:8080/cover.jpg'
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the song title correctly', () => {
+    const titleElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+    expect(titleElement.textContent).toContain('Test Song Title');
+  });
+
+  it('should display artist name correctly', () => {
+    const artistElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(artistElement.textContent).toContain('Test Artist');
+  });
+
+  it('should call playerStore play() when clicked', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+
+    button.triggerEventHandler('click', new Event('click'));
+
+    expect(mockPlayerStore.play).toHaveBeenCalledWith(mockSong);
+  })
 });
